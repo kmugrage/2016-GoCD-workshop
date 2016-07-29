70561fe30c3a8575ec11d37d5b8d660375630178
Ken Mugrage <kmugrage@thoughtworks.com>
2016-07-29 11:22:57 -0700

    spacing

--- update_config.sh
+++ update_config.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-        # Put our config file in place
-        OLD_ID=$(sed -n 's/.*serverId="\([^"]*\)".*/\1/p' /etc/go/cruise-config.xml)
-        sed -e 's/serverId="[^"]*"/serverId="'$OLD_ID'"/' /vagrant/new_config.xml >new-id-old-config.xml
-        mv new-id-old-config.xml /etc/go/cruise-config.xml
-	chown go:go /etc/go/cruise-config.xml
+# Put our config file in place
+OLD_ID=$(sed -n 's/.*serverId="\([^"]*\)".*/\1/p' /etc/go/cruise-config.xml)
+sed -e 's/serverId="[^"]*"/serverId="'$OLD_ID'"/' /vagrant/new_config.xml >new-id-old-config.xml
+mv new-id-old-config.xml /etc/go/cruise-config.xml
+chown go:go /etc/go/cruise-config.xml
