0129ad87e71f5963da1818bf4579ce6d108a3f56
Ken Mugrage <kmugrage@thoughtworks.com>
2017-03-14 13:28:41 -0700

    output message about GoCD startup time

---
+++ startup_message.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "#################################"
+echo "#                               #"
+echo "#   NOTE: It may take several   #"
+echo "#   minutes for GoCD to start   #"
+echo "#                               #"
+echo "#################################"
